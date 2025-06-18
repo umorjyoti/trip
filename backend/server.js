af7aa6d56ad570b593472670903162e8c80530fc
umorjyoti <umorjyotichetia@gmail.com>
2025-06-18 22:32:24 +0530

    host added cors 1

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:5001', 'https://trip-ctuq.onrender.com',"89.117.157.153"],
+  origin: [process.env.FRONTEND_URL || 'http://localhost:5001', 'https://trip-ctuq.onrender.com',"89.117.157.153","https://bengalurutrekkers.in"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: [
