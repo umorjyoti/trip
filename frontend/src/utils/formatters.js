5045b4e545815738e185e0278be7ac4e079ff16f
umorjyoti <umorjyotichetia@gmail.com>
2025-07-20 09:43:58 +0530

    rounding up issue fixed

--- frontend/src/utils/formatters.js
+++ frontend/src/utils/formatters.js
@@ -2,8 +2,8 @@ export const formatCurrency = (amount) => {
   return new Intl.NumberFormat('en-IN', {
     style: 'currency',
     currency: 'INR',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
   }).format(amount);
 };
 
