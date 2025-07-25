3e954f2011ff2357020c70fd9cd78291ec2e1572
umorjyoti <umorjyotichetia@gmail.com>
2025-07-25 17:31:49 +0530

    region creation failing

--- backend/models/Region.js
+++ backend/models/Region.js
@@ -8,7 +8,6 @@ const RegionSchema = new mongoose.Schema({
   },
   slug: {
     type: String,
-    required: true,
     unique: true,
     trim: true,
     lowercase: true
@@ -77,14 +76,30 @@ RegionSchema.pre('save', function(next) {
 });
 
 // Pre-save middleware to generate slug from name
-RegionSchema.pre('save', function(next) {
-  if (this.isModified('name') || this.isNew) {
-    this.slug = this.name
+RegionSchema.pre('save', async function(next) {
+  // Always generate slug if name is provided
+  if (this.name) {
+    let baseSlug = this.name
       .toLowerCase()
       .replace(/[^a-z0-9\s-]/g, '') // Remove special characters except spaces and hyphens
       .replace(/\s+/g, '-') // Replace spaces with hyphens
       .replace(/-+/g, '-') // Replace multiple hyphens with single hyphen
       .replace(/^-+|-+$/g, ''); // Remove leading/trailing hyphens
+    
+    // Handle uniqueness
+    let slug = baseSlug;
+    let counter = 1;
+    
+    while (true) {
+      const existingRegion = await this.constructor.findOne({ slug: slug, _id: { $ne: this._id } });
+      if (!existingRegion) {
+        break;
+      }
+      slug = `${baseSlug}-${counter}`;
+      counter++;
+    }
+    
+    this.slug = slug;
   }
   next();
 });
